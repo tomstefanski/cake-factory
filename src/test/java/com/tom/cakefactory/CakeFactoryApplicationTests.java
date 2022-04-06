88276b9d7c34c5a068d8f58f0136990cbe25eb92
Tom Stefanski <stefanski.t@pg.com>
2022-04-06 22:34:28 +0200

    milestone1

---
+++ src/test/java/com/tom/cakefactory/CakeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.cakefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
